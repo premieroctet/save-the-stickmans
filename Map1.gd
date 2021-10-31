6d7d5cc02b48650755f6c2281c776430c7fa17f7
Thibault Lenclos <tlenclos@users.noreply.github.com>
2021-11-01 00:04:28 +0100

    Improve line drawing

--- Map1.gd
+++ Map1.gd
@@ -1,25 +1,52 @@
 extends Node2D
+# TODO
+# - Add timeout on line, to delete it after X seconds => Maybe use a script just for a line instance ?
+# - Implement jump and movements of stickmans
+# - Add door at the end of the level
+# - Add counter of saved stickmans
 
-onready var line = $Line2D
 onready var fpsLabel = $FpsLabel
 
-var segments = []
+export var lineTimeout = 2
+export var lineWidth = 3
 
-func _physics_process(delta):
+var line: Line2D
+var lineBody: StaticBody2D
+
+func _ready():
+	print("ready")
+
+func _physics_process(_delta):
 	fpsLabel.set_text(str(Engine.get_frames_per_second()))
 
-func _input(event: InputEvent) -> void:
-	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("click"):
+		line = create_line()
+		
+	if Input.is_action_pressed("click"):
 		line.add_point(get_local_mouse_position())
-		# TODO Create collision & shape for the lines (see https://godotengine.org/qa/67407/adding-collision-detection-to-line2d-collisionpolygon2d)
-	
+		
+		# Creates collision & shape for the lines (see https://godotengine.org/qa/67407/adding-collision-detection-to-line2d-collisionpolygon2d)
 		var n = line.points.size()
 		for i in range(n-1):
-			segments.append(create_segment(line.points[i], line.points[i+1]))
+			lineBody.add_child(create_segment(line.points[i], line.points[i+1]))
 			
-		for s in segments:
-			$Line2D/LineBody.add_child(s)
+	# Remove lines after a timeouts
+#	if Input.is_action_just_released("click"):
+#		yield(get_tree().create_timer(lineTimeout), "timeout")
+#		line.queue_free()
 		
+func create_line() -> Line2D:
+	var line = Line2D.new()
+	line.width = lineWidth
+	line.default_color = Color(0, 0, 0)
+	
+	lineBody= StaticBody2D.new()
+	line.add_child(lineBody)
+	add_child(line)	
+	
+	return line
+	
 # Create a 2 collision shape from 2 points
 func create_segment(p1 : Vector2, p2 : Vector2) -> CollisionShape2D:
 	var collision = CollisionShape2D.new()
--- Stickman.gd
+++ Stickman.gd
@@ -2,6 +2,8 @@ extends RigidBody2D
 
 export (int) var speed = 1
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	apply_central_impulse(Vector2(speed, 0))
-
+	
+	# TODO Implement proper control input
+ 
