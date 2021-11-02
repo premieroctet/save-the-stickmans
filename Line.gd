3d0d49ec04b02bf07b4cb5459c33db76b53e7fe8
Thibault Lenclos <tlenclos@users.noreply.github.com>
2021-11-02 20:10:58 +0100

    Remove line after timeout

--- Line.gd
+++ Line.gd
@@ -1,31 +1,21 @@
 extends Line2D
 
-var lineBody: StaticBody2D
+onready var lineBody: StaticBody2D = $StaticBody2D
 
-export (int) var lineTimeout = 2
+export (int) var lineTimeout = 5
 export (int) var lineWidth = 3
 
-# Create line body
 func _ready():
-	width = lineWidth
-	default_color = Color(0, 0, 0)
-	
-	lineBody = StaticBody2D.new()
-	add_child(lineBody)
+	yield(get_tree().create_timer(lineTimeout), "timeout")
+	queue_free()
 
 func add_point_to_line(position):
 	add_point(position)
 	
 	# Creates collision & shape for the lines (see https://godotengine.org/qa/67407/adding-collision-detection-to-line2d-collisionpolygon2d)
 	var n = points.size()
-	print(lineBody)
 	for i in range(n-1):
 		lineBody.add_child(create_segment(points[i], points[i+1]))
-
-	# Remove lines after a timeouts
-#	if Input.is_action_just_released("click"):
-#		yield(get_tree().create_timer(lineTimeout), "timeout")
-#		line.queue_free()
 
 # Create a 2 collision shape from 2 points
 func create_segment(p1 : Vector2, p2 : Vector2) -> CollisionShape2D:
--- Map1.gd
+++ Map1.gd
@@ -1,27 +1,26 @@
 extends Node2D
 # TODO
-# - Add timeout on line, to delete it after X seconds => Maybe use a script just for a line instance ?
 # - Implement jump and movements of stickmans
 # - Add door at the end of the level
 # - Add counter of saved stickmans
 
 onready var fpsLabel = $FpsLabel
+onready var Line =  preload("Line.tscn")
 
+var line
 export var lineTimeout = 2
 export var lineWidth = 3
 
-var line: Line2D
 var lineBody: StaticBody2D
-
-func _ready():
-	print("ready")
 
 func _physics_process(_delta):
 	fpsLabel.set_text(str(Engine.get_frames_per_second()))
 
 func _input(_event: InputEvent) -> void:
-#	if Input.is_action_just_pressed("click"):
-#		line = $Line.duplicate()
+	if Input.is_action_just_pressed("click"):
+		print('just pressed click')
+		line = Line.instance()
+		add_child(line)
 		
 	if Input.is_action_pressed("click"):
 		line.add_point_to_line(get_local_mouse_position())
