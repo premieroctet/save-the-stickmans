0289c7c54eb5f11efa759409168b6026e42da684
Thibault Lenclos <tlenclos@users.noreply.github.com>
2021-11-06 14:41:02 +0100

    Stickmans movements and end game detection

--- Map1.gd
+++ Map1.gd
@@ -1,26 +1,51 @@
 extends Node2D
 # TODO
-# - Implement jump and movements of stickmans
-# - Add door at the end of the level
-# - Add counter of saved stickmans
+# - Start and end game
+# - Stickman capacity : repulse
+# - Random stickman becomes a zombie
 
 onready var fpsLabel = $FpsLabel
+onready var scoreLabel = $ScoreValue
 onready var Line =  preload("Line.tscn")
 
-var line
 export var lineTimeout = 2
 export var lineWidth = 3
 
+var line
 var lineBody: StaticBody2D
+var savedStickmans = 0
+var deadStickmans = 0
 
 func _physics_process(_delta):
 	fpsLabel.set_text(str(Engine.get_frames_per_second()))
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("click"):
-		print('just pressed click')
 		line = Line.instance()
 		add_child(line)
 		
 	if Input.is_action_pressed("click"):
 		line.add_point_to_line(get_local_mouse_position())
+
+func _on_EndArea_body_entered(body):
+	if body.is_in_group("stickmans"):
+		body.queue_free()
+		savedStickmans += 1
+		scoreLabel.set_text(str(savedStickmans))
+		check_if_game_ended()
+
+func _on_LevelArea_body_exited(body):
+	if body.is_in_group("stickmans"):
+		body.queue_free()
+		deadStickmans += 1
+		check_if_game_ended()
+		
+func check_if_game_ended() -> bool: 
+	if get_tree().get_nodes_in_group('stickmans').size() == 1: # TODO Why is there still one node ?
+		# TODO Restart game, reposition stickmans
+		savedStickmans = 0
+		deadStickmans = 0
+		scoreLabel.set_text(str(savedStickmans))
+		return true
+		
+	return false
--- Stickman.gd
+++ Stickman.gd
@@ -1,11 +1,27 @@
 extends KinematicBody2D
 
-export (int) var speed = 20
 export (int) var gravity = 1200
+# export (int) var run_speed = 60
+export (int) var run_speed = 300
+export (int) var jump_speed = -400
 
 var velocity = Vector2()
+const UP = Vector2(0, -1)
+
+func get_input():
+	velocity.x = 0
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_just_pressed('ui_accept')
+
+	if jump and is_on_floor():
+		velocity.y = jump_speed
+	if right:
+		velocity.x += run_speed
+	if left:
+		velocity.x -= run_speed
 
 func _physics_process(delta):
+	get_input()
 	velocity.y += gravity * delta
-	velocity.x = speed;
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, UP)
