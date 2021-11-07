a7cc5403d824628e67ecd84cb1c0108cbd2a6242
Thibault Lenclos <tlenclos@users.noreply.github.com>
2021-11-07 22:48:37 +0100

    Improve traps and start scream implem

--- Stickman.gd
+++ Stickman.gd
@@ -1,18 +1,18 @@
 extends KinematicBody2D
 
-export (int) var gravity = 1200
-# export (int) var run_speed = 60
-export (int) var run_speed = 300
-export (int) var jump_speed = -400
+export (int) var gravity = 800
+export (int) var run_speed = 150
+export (int) var jump_speed = -350
 
 var velocity = Vector2()
-const UP = Vector2(0, -1)
 
 func get_input():
 	velocity.x = 0
+	
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
 	var jump = Input.is_action_just_pressed('ui_accept')
+	var scream = Input.is_action_just_pressed('scream')
 
 	if jump and is_on_floor():
 		velocity.y = jump_speed
@@ -20,8 +20,16 @@ func get_input():
 		velocity.x += run_speed
 	if left:
 		velocity.x -= run_speed
-
+	if scream:
+		$ScreamLabel.visible = true
+#		for stickman in get_tree().get_nodes_in_group('stickmans'):
+#			if stickman != self:
+#				stickman.velocity.bounce(stickman.velocity)
+			
+		yield(get_tree().create_timer(2), "timeout")
+		$ScreamLabel.visible = false
+			
 func _physics_process(delta):
-	get_input()
+	get_input() 
 	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, UP)
+	move_and_slide(velocity, Vector2.UP)
--- Trap.gd
+++ Trap.gd
@@ -4,14 +4,12 @@ export (float) var rotation_speed = 0.5
 export (int) var rotation_dir = 1
 
 export (int) var movement_speed = 10
-export (int) var max_movement_limit = 10
+export (int) var max_movement_limit = 20
 export (int) var movement_dir = 1
 
 var velocity = Vector2()
 var velocity_value = 1
 var initial_position = Vector2()
-
-# TODO No collision with drawing
 
 func _ready():
 	initial_position = position
@@ -20,16 +18,11 @@ func _physics_process(delta):
 	if rotation_speed > 0:
 		rotation += rotation_dir * rotation_speed * delta
 	
-	print(initial_position, position)
-	
 	if movement_speed > 0:
 		velocity = Vector2()
-		
 		velocity.x += velocity_value
 		
-		# TODO Check limit to change velocity value and make the platform go back
-		# TODO Use movemnet dir to change velocity direction
-		if initial_position.x - position.x > max_movement_limit:
+		if abs(position.x - initial_position.x) > max_movement_limit:
 			velocity_value = -1 
 		elif position.x < initial_position.x:
 			velocity_value = 1
