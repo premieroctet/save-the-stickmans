9bb439d07535165a745f17cf3e245f89d1b460ca
Thibault Lenclos <tlenclos@users.noreply.github.com>
2021-11-09 23:14:35 +0100

    ref #4 basic fear implementation

--- Stickman.gd
+++ Stickman.gd
@@ -1,10 +1,15 @@
 extends KinematicBody2D
 
+export (bool) var can_move = true
 export (int) var gravity = 800
 export (int) var run_speed = 150
 export (int) var jump_speed = -350
+export (int) var fear_impulse = 5000
+export (Vector2) var fear_recover = Vector2(100, 100)
+export (Vector2) var fear_distance = Vector2(30, 30)
 
 var velocity = Vector2()
+var fearVelocity = Vector2()
 
 func get_input():
 	velocity.x = 0
@@ -22,14 +27,36 @@ func get_input():
 		velocity.x -= run_speed
 	if scream:
 		$ScreamLabel.visible = true
-#		for stickman in get_tree().get_nodes_in_group('stickmans'):
-#			if stickman != self:
-#				stickman.velocity.bounce(stickman.velocity)
+		for stickman in get_tree().get_nodes_in_group('stickmans'):
+			if stickman != self:
+				var distance: Vector2 = (stickman.position - position)
+				if distance.abs().x < fear_distance.x:
+					stickman.fear(distance)
 			
 		yield(get_tree().create_timer(2), "timeout")
 		$ScreamLabel.visible = false
 			
+func fear(distance: Vector2):
+	fearVelocity = Vector2(0, 0)
+	fearVelocity = distance.normalized() * fear_impulse
+	
 func _physics_process(delta):
-	get_input() 
-	velocity.y += gravity * delta
+	if can_move == true:
+		get_input() 
+	
+	# Gravity
+	if velocity.y < gravity:
+		velocity.y += gravity * delta
+	
+	# Fear
+	# TODO Improve math
+	if fearVelocity < Vector2(-1, -1) or fearVelocity > Vector2(1, 1):
+		if fearVelocity.x > 0:
+			fearVelocity -= fear_recover
+		else:
+			fearVelocity += fear_recover
+			
+		move_and_slide(fearVelocity * delta, Vector2.UP)
+	
+	# Movements
 	move_and_slide(velocity, Vector2.UP)
