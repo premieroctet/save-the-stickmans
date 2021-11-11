ac4d9818e3de5344f1481f862b9cac9f590cf399
Thibault <thibault@premieroctet.com>
2021-11-11 15:34:24 +0100

    Update server

--- server/Server.gd
+++ server/Server.gd
@@ -1,31 +1,50 @@
 extends Node
 
-var SERVER_PORT = 1100
+var SERVER_PORT = 8080
 var MAX_PLAYERS = 50
-var players_info = {}
 
-# Info we send to other players
-var my_info = { name = "Johnson Magenta", favorite_color = Color8(255, 0, 255) }
+var players_ended = []
+var players_positions = {}
+var line_segments: PoolVector2Array
 
 func _ready():
 	var server = NetworkedMultiplayerENet.new()
-	server.create_server(SERVER_PORT, MAX_PLAYERS)
+	var server_created = server.create_server(SERVER_PORT, MAX_PLAYERS)
 	get_tree().set_network_peer(server)
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
-	print('server started')
+	print('server started ', server_created)
+
+func _physics_process(delta):
+	send_world_state()
+	
+func is_all_players_ended():
+	return players_ended.size() == players_positions.size()
 
 func _player_connected(id):
 	print('Player connected', id)
-	# Called on both clients and server when a peer connects. Send my info to it.
-	rpc_id(id, "register_player", my_info)
 
 func _player_disconnected(id):
-	print('Player disconnect', id)
-	players_info.erase(id) # Erase player from info.
+	print('Player disconnect ', id)
+	players_positions.erase(id)
+	
+func send_world_state():
+	rpc_unreliable('sync_players', players_positions)
 
 remote func register_player(info):
-	# Get the id of the RPC sender.
 	var id = get_tree().get_rpc_sender_id()
-	# Store the info
-	players_info[id] = info
+	players_positions[id] = info
+	
+remote func player_end():
+	var id = get_tree().get_rpc_sender_id()
+	players_ended.append(id)
+	
+	if is_all_players_ended():
+		rpc('reset')
+		players_ended = []
+		players_positions = {}
+
+remote func draw(point):
+	var id = get_tree().get_rpc_sender_id()
+	line_segments.append(point)
+	rpc_unreliable('draw', line_segments)
